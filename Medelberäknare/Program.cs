2e24044e30f0dea001a0eafb1f9a496b11d2c3db
MellerPeller <melker.eriksson@skola.sala.se>
2024-08-28 13:33:55 +0200

    Add project files.

---
+++ Medelberäknare/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Medelberäknare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double value = 0;
+            double divider =0;
+
+
+            while (true) 
+            { 
+                value = value + Convert.ToDouble(Console.ReadLine());
+                divider++;
+                Console.WriteLine("Medelvärdet är " + (value/divider));
+             
+            }
+
+
+
+
+
+        }
+    }
+}
